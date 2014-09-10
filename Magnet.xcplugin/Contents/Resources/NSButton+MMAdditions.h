373e19508844bf7e07f07d3a0dcf28354bf9df90
Pritesh Shah <pritesh.shah@magnet.com>
2014-09-10 11:21:48 -0700

    Adding RC2 version

---
+++ Magnet.xcplugin/Contents/Resources/NSButton+MMAdditions.h
@@ -0,0 +1,9 @@
+/**
+ * Copyright (c) 2012-2014 Magnet Systems, Inc. All rights reserved.
+ */
+
+@interface NSButton (MMAdditions)
+
+@property (assign, nonatomic, getter = isEnabled, setter = setEnabled:) BOOL enabled;
+
+@end
